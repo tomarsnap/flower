65dfcbf0373667857a60ba093b7e29f4e1a33e9f
tomarsnap <42405521+tomarsnap@users.noreply.github.com>
2019-09-24 14:20:36 +0300

    Move functionality to event class

--- flower/api/events.py
+++ flower/api/events.py
@@ -3,8 +3,6 @@ from __future__ import absolute_import
 import sys
 
 from ..api import BaseWebSocketHandler
-from ..app import Flower
-
 
 class EventsApiHandler(BaseWebSocketHandler):
     def open(self, task_id=None):
@@ -15,37 +13,7 @@ class EventsApiHandler(BaseWebSocketHandler):
     def send_message(cls, event):
         for l in cls.listeners:
             if not l.task_id or l.task_id == event['uuid']:
-                event["actives"] = cls.get_events_state()
                 l.write_message(event)
-    
-    @classmethod
-    def get_events_state(cls):
-        state = Flower.events.state
-        # workers = OrderedDict()
-        actives = []
-        for name, worker in sorted(state.workers.items()):
-            counter = state.counter[name]
-            started = counter.get('task-started', 0)
-            processed = counter.get('task-received', 0)
-            failed = counter.get('task-failed', 0)
-            succeeded = counter.get('task-succeeded', 0)
-            retried = counter.get('task-retried', 0)
-            active = started - succeeded - failed - retried
-            # if active < 0:
-            #     active = 'N/A'
-            actives.append(active)
-            #workers[name] = dict(
-            #    name=name,
-            #    status=worker.alive,
-            #    active=active,
-            #    processed=processed,
-            #    failed=failed,
-            #    succeeded=succeeded,
-            #    retried=retried,
-            #    loadavg=getattr(worker, 'loadavg', None))
-        return sum(actives)
-        
-
 
 EVENTS = ('task-sent', 'task-received', 'task-started', 'task-succeeded',
           'task-failed', 'task-revoked', 'task-retried', 'task-custom')
